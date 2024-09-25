42d1012ffc6cd7fc715d847ca361cf3c5aa051ac
amainooti <amainosamuel1@gmail.com>
2024-09-25 09:56:52 +0100

    changes

---
+++ src/cat.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from "@nestjs/common";
+
+
+
+@Controller("cat")
+export class CatController{
+
+    @Get()
+    findAll(): string{
+        return "This action returns all cats"
+    }
+}
