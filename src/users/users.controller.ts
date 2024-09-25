93e35aa81ef7a8eaaeb57c17e4a351c8e8254266
amainooti <amainosamuel1@gmail.com>
2024-09-25 10:14:25 +0100

    ...

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+
+    constructor(private readonly userService: UsersService){
+
+    }
     
 @Get()
-  findAll() {
-    return 'This action returns all users';
+  findAll(@Query("role") role: "ADMIN" | "ENGINEER" | "AUDITOR") {
+    return this.userService.getUsers();
   }
 
+
+  @Get(":id")
+  findOne(@Param("id") id: string){
+    return { id }
+  }
+
+
+  @Post()
+  create(@Body() user: {}){
+    return user
+  }
+
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() userUpdate: {}){
+    return {id, ...userUpdate}
+  }
+
+  @Delete(":id")
+  delete(@Param("id") id: string) {
+    return {id}
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    
+    getUsers(){
+        return ["user1", "user2"]
+    }
+}
